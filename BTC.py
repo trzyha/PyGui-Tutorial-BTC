c74634bfa0131934fbb08b122e4bfa210fd237da
trzyha <39124933+trzyha@users.noreply.github.com>
2019-01-25 23:15:15 +0100

    Add files via upload

--- BTC.py
+++ BTC.py
@@ -25,12 +25,20 @@ class SeaofBTCapp(tk.Tk):
         frame = self.frames[cont]
         frame.tkraise()
 
+
+def qf(param):
+    print(param)
+
 class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Start Page", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
+
+        button1 = tk.Button(self, text="Visit Page 1",
+                            command=lambda: qf("see this worked")) #lambda to pass the argument to the function
+        button1.pack()
 
 app = SeaofBTCapp()
 app.mainloop()
